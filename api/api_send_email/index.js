732e16c6c733cd0292e0dec71a283474f86d6a6e
Oleksandr Nykytin <keeper@ninydev.com>
2023-06-23 22:36:09 +0300

    api.send.email

--- api/api_send_email/index.js
+++ api/api_send_email/index.js
@@ -15,65 +15,64 @@ const RABBITMQ_QUEUE_SEND_EMAIL = process.env.RABBITMQ_QUEUE_SEND_EMAIL || 'send
 const SERVER_NAME = process.env.SERVER_NAME || 'api.send.email'
 const SERVER_PORT = process.env.SERVER_PORT || 3000
 
-import amqp from 'amqplib'
+/**
+ * Modules
+ */
+import amqp from 'amqplib/callback_api.js'
 import http from 'http'
+import { v4 } from 'uuid'
 
-let connection
-let channel
+/**
+ * Step 1
+ * Connect to rabbitMQ server
+ */
+amqp.connect(RABBITMQ_CONNECTION_URI,{},
+    async (errorConnect, connection) => {
+        if (errorConnect) {
+            console.error(errorConnect)
+            process.exit(-1)
+        }
+        console.debug("connect RabbitMQ ok")
 
-(async () => {
-    try {
-        connection = await amqp.connect(RABBITMQ_CONNECTION_URI)
-        console.debug(`${SERVER_NAME}: connection to ${RABBITMQ_CONNECTION_URI}`)
-        channel = await connection.createChannel()
-        await channel.assertQueue(RABBITMQ_QUEUE_SEND_EMAIL)
-        console.debug(`${SERVER_NAME}: channel to RabbitMQ server ok`)
-
-        http.createServer((request, response) =>{
-            let msg = {
-                userName: 'Nykytin',
-                userEmail: 'keeper@ninydev.com'
+        /**
+         * Step 2
+         * Create rabbitMQ channel
+         */
+        connection.createChannel((errorChannel, channel) => {
+            if (errorChannel) {
+                console.error(errorChannel)
+                process.exit(-1)
             }
-            channel.sendToQueue(RABBITMQ_QUEUE_SEND_EMAIL, Buffer.from('test'))
-            //channel.sendToQueue(RABBITMQ_QUEUE_SEND_EMAIL, Buffer.from(JSON.stringify(msg)))
-            response.end()
-            }).listen(SERVER_PORT, () => {
-                console.debug(`${SERVER_NAME}: http server start`)
+            channel.assertQueue(RABBITMQ_QUEUE_SEND_EMAIL)
+            console.debug("create RabbitMQ channel ok")
+
+            /**
+             * Step 3
+             * Create http server
+             */
+            http.createServer((request, response) =>{
+                /**
+                 * Step 4
+                 * when front send data create msg id <UUID v4>
+                 */
+                let msg = {
+                    'id' : v4(),
+                    'email' : 'keeper@ninydev.com', //request.body.email,
+                    'name' : 'Oleksandr Nykytin' //request.body.name
+                }
+                console.log(msg)
+
+                /**
+                 * Step 5
+                 * send message to RabbitMQ queue
+                 */
+                channel.sendToQueue(RABBITMQ_QUEUE_SEND_EMAIL, Buffer.from(JSON.stringify(msg)))
+
+                /**
+                 * Step 6
+                 * send msg.id to front
+                 */
+                response.end(msg.id)
+            }).listen(3000)
         })
-    }
-    catch (err) {
-        console.error(err)
-    }
-    finally {
-        if (connection) await connection.close()
-    }
-})()
-
-
-// amqp.connect(`amqp://${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}@${RABBITMQ_SERVER}:${RABBITMQ_PORT}`,
-//     (errorConnect, connection) => {
-//         if (errorConnect) {
-//             console.error(errorConnect)
-//             process.exit(-1);
-//         }
-//         console.debug("connect RabbitMQ ok")
-//
-//         connection.createChannel((errorChannel, channel) => {
-//             if (errorChannel) {
-//                 console.log(errorChannel)
-//                 process.exit(-1);
-//             }
-//             console.debug("create RabbitMQ channel ok")
-//
-//             channel.assertQueue(RABBITMQ_QUEUE_SEND_EMAIL);
-//
-//             http.createServer((request, response) =>{
-//                 console.debug(request)
-//                 response.end()
-//             }).listen(3000);
-//         })
-//     })
-//     .catch(exception => {
-//         console.error(exception)
-//     })
-//
+    })
